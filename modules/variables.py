9d0d973ccfdd60d6573e5a8708915aca63c83683
augustin64 <augustin.lucas64@gmail.com>
2022-05-24 22:19:15 +0200

    Update modules/variables

--- modules/variables.py
+++ modules/variables.py
@@ -5,14 +5,26 @@ Module loading variables from config file
 import configparser
 
 CONFIG_PATH = "./config"
+
 config = configparser.ConfigParser()
 config.read(CONFIG_PATH)
-
-MotPath = config["DEFAULT"]["motpath"]
-LogPath = config["DEFAULT"]["logpath"]
-SuccessLink = config["DEFAULT"]["successlink"]
-ErrorLink = config["DEFAULT"]["errorlink"]
-
-embeds = config["DEFAULT"]["embeds"] == "True"
-Headless = config["DEFAULT"]["headless"] == "True"
-Log = config["DEFAULT"]["log"] == "True"
+#path configurations
+MotPath = config["PATH"]["motpath"]
+LogPath = config["PATH"]["logpath"]
+#discord configurations
+SuccessLink = config["DISCORD"]["successlink"]
+ErrorLink = config["DISCORD"]["errorlink"]
+#base settings
+FidelityLink = config["SETTINGS"]["FidelityLink"]
+embeds = config["SETTINGS"]["embeds"] == "True" #print new point value in an embed
+Headless = config["SETTINGS"]["headless"] == "True"
+#proxy settings
+proxy_enabled = config["PROXY"]["enabled"] == "True"
+proxy_adress = config["PROXY"]["url"] 
+proxy_port = config["PROXY"]["port"]
+#MySQL settings
+sql_enabled = config["SQL"]["enabled"] == "True"
+sql_usr = config["SQL"]["usr"]
+sql_pwd = config["SQL"]["pwd"]
+sql_host = config["SQL"]["host"]
+sql_database = config["SQL"]["database"]
