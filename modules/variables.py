5739c8922fa168e0276b182a1a69eef483dac0a6
augustin64 <augustin.lucas64@gmail.com>
2022-05-26 17:15:07 +0200

    Update module/variables

--- modules/variables.py
+++ modules/variables.py
@@ -4,7 +4,7 @@ Module loading variables from config file
 """
 import configparser
 
-CONFIG_PATH = "./config"
+CONFIG_PATH = "config"
 
 config = configparser.ConfigParser()
 config.read(CONFIG_PATH)
@@ -14,13 +14,14 @@ LogPath = config["PATH"]["logpath"]
 #discord configurations
 SuccessLink = config["DISCORD"]["successlink"]
 ErrorLink = config["DISCORD"]["errorlink"]
+discord_enabled = config["DISCORD"]["enabled"]
 #base settings
 FidelityLink = config["SETTINGS"]["FidelityLink"]
 embeds = config["SETTINGS"]["embeds"] == "True" #print new point value in an embed
 Headless = config["SETTINGS"]["headless"] == "True"
 #proxy settings
 proxy_enabled = config["PROXY"]["enabled"] == "True"
-proxy_adress = config["PROXY"]["url"] 
+proxy_address = config["PROXY"]["url"]
 proxy_port = config["PROXY"]["port"]
 #MySQL settings
 sql_enabled = config["SQL"]["enabled"] == "True"
